fce16e0f9f5d8439b1f9dc72631f04c5c614175e
Yujin Cho <yujinjcho@gmail.com>
2016-05-04 22:41:18 -0700

    testing match comprehension concat

--- app/views.py
+++ app/views.py
@@ -189,7 +189,7 @@ def find_game():
   
   current_matches = Matches.query.filter((Matches.challenger_id==session['user_id']) | (Matches.challenged_id==session['user_id']))
   match_test = [match.id for match in current_matches]
-  return str(len(match_test))
+  return ', '.join(str(x) for x in match_test)
 
   matches = [match.challenged_id for match in current_matches if match.challenger_id == session['user_id']] + \
             [match.challenger_id for match in current_matches if match.challenged_id == session['user_id']]
